abbe1ea6f179b0cb8e47f431c6e2af2cc10e3a93
Sharif-Miah <farhansharif1410@gmail.com>
2024-10-27 21:18:22 +0600

    no task found added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { useState } from "react";
 import "./App.css";
+import { SortContext } from "./context/intex";
 import Page from "./Page/Page";
 
 function App() {
+  const [isSort, setIsSort] = useState(null);
+
   return (
     <div className="bg-gray-900 text-white">
-      <Page />
+      <SortContext.Provider value={{ isSort, setIsSort }}>
+        <Page />
+      </SortContext.Provider>
     </div>
   );
 }
